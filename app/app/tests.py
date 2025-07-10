d0d22b7454432e2403a03aa95bfecc84d5a0b153
Khawar Shahzad <khawar.shahzad@arbisoft.com>
2025-07-10 19:53:50 +0500

    Sample unit test added

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test calc methods"""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
