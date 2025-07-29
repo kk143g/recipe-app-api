a9e58c6687f74293e37e1ed298d89e8d0d73adf0
Khawar Shahzad <khawar.shahzad@arbisoft.com>
2025-07-29 17:04:19 +0500

    added health check API

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,8 +23,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from core import views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     # It will generate schema file for our API
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     # This will tell swagger to use above schema for it's GUI
